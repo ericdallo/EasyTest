e1949b53ffc9dae643c90fa6cd51dc8b1e38335c
Eric Dallo <ericdallo06@hotmail.com>
2016-04-30 22:15:35 -0300

    First commit

---
+++ mobile/src/main/java/com/aggregator/easytest/EasyTestActivityFragment.java
@@ -0,0 +1,22 @@
+package com.aggregator.easytest;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class EasyTestActivityFragment extends Fragment {
+
+    public EasyTestActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_easy_test, container, false);
+    }
+}
